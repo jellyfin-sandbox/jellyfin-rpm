4d0f1a312202847b19533d3137cfffb137ce601d
K3rnelPan1c <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-11-05 23:24:59 +0100

    add: initial server rpm build workflow

---
+++ server/buildRpmInContainer.sh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -exu
+
+HOME="$(pwd)"
+SOURCE_DIR="$(pwd)/jellyfin-code"
+RPMBUILD_HOME="$(pwd)/rpmbuild"
+MANUAL_PROVIDED_DEPENDENCIES="true"
+export HOME
+export SOURCE_DIR
+export RPMBUILD_HOME
+export MANUAL_PROVIDED_DEPENDENCIES
+
+mkdir rpmbuild
+
+pushd jellyfin-code
+
+# build rpm
+make -f fedora/Makefile srpm outdir="${RPMBUILD_HOME}/SRPMS"
+rpmbuild -rb "${RPMBUILD_HOME}"/SRPMS/jellyfin-*.src.rpm
+
+popd
+
+# move the artifacts
+mkdir out
+mv "${RPMBUILD_HOME}"/RPMS/noarch/jellyfin-*.rpm "${RPMBUILD_HOME}"/SRPMS/jellyfin-*.src.rpm out
